7361bb92910a52dfb97837f0bd308a67f81ca027
harlleenn <harleenkukreja406@gmail.com>
2026-03-15 16:00:09 +0530

    added content in spotlight

--- src/demo/App.jsx
+++ src/demo/App.jsx
@@ -3,27 +3,30 @@ import Sidebar from './components/Dashboard/Sidebar'
 import Navbar from "./components/Dashboard/Navbar";
 import Content from './components/Dashboard/main-content/Content'
 import Spotlight from "../lib/components/Spotlight"
-import steps from "./Steps";
+import steps from "../lib/components/Steps";
 
 function App() {
   const [tourActive, setTourActive] = useState(false);
   return (
-    <div className="flex h-screen" >
+  <>
+    <div className='md:hidden flex h-screen items-center justify-center p-8 text-center text-white bg-black'>
+      <p>This demo is best viewed on desktop. Mobile support coming soon.</p>
+    </div>
+    <div className='hidden md:flex h-screen'>
       <Sidebar />
-
       <div className="flex flex-1 flex-col">
         <Navbar />
         <Content onStartTour={() => setTourActive(true)} />
       </div>
-
       {tourActive && (
-        <Spotlight 
-        steps={steps} 
-        onFinish={() => setTourActive(false)}
+        <Spotlight
+          steps={steps}
+          onFinish={() => setTourActive(false)}
         />
       )}
     </div>
-  );
+  </>
+)
 }
 
 export default App;
